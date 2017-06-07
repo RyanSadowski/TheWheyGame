b20fc691c8cabf399a6bc72660bf196fab384506
RyanSadowski <nemosaurus@gmail.com>
2017-06-07 02:30:19 -0700

    register user

---
+++ Assets/Scripts/User/registerButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class registerButton : MonoBehaviour {
+	public Button button;
+
+	// Use this for initialization
+	void Start () {
+		Button btn = button.GetComponent<Button>();
+		btn.onClick.AddListener(goRegister);
+	}
+	void goRegister(){
+		//Application.LoadLevel("Register"); deprecated
+		SceneManager.LoadScene ("Register");
+	}
+}
---
+++ Assets/Scripts/User/registerForm.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.Networking;
+
+
+public class registerForm : MonoBehaviour {
+	public Button button;
+	public Text usernameField;
+	public Text passwordField;
+	public Text firstNameField;
+	public Text lastNameField;
+	public Text emailField;
+	private string username;
+	private string password;
+	private string firstName;
+	private string lastName;
+	private string email;
+	//public string result;
+	public string url = "http://localhost:3000/user/setup";
+
+	// Use this for initialization
+	void Start () {
+		Button btn = button.GetComponent<Button>();
+		btn.onClick.AddListener(Submit);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void Submit() {
+		
+		username = usernameField.text.ToString ();
+		password = passwordField.text.ToString ();
+		firstName = firstNameField.text.ToString ();
+		lastName = lastNameField.text.ToString ();
+		email = emailField.text.ToString ();
+
+	
+
+		WWWForm form = new WWWForm();
+		form.AddField("username", username);
+		form.AddField("password", password);
+		form.AddField("firstName", firstName);
+		form.AddField("lastName", lastName);
+		form.AddField("email", email);
+
+		StartCoroutine("CreateUser", form);
+	}
+
+	IEnumerator CreateUser(WWWForm form) {
+		UnityWebRequest www = UnityWebRequest.Post("http://localhost:3000/user/setup", form);
+		yield return www.Send();
+		if(!string.IsNullOrEmpty(www.error)) {
+			print( "Error : " + www.error );
+		} else {
+			Debug.Log("yassss");
+			Debug.Log(www.downloadHandler.text);
+		}
+	}
+
+
+}
